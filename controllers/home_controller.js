579cef4bad06d29cf02068272a947dba851feece
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-09 12:44:46 +0530

    Done with making all the functionalities now CSS and Styling need to be improved

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,35 @@
-module.exports.home=function(req,res){
-    return res.render("home",{
-        title:"Home Page"
-    })
+const Product = require("../models/products");
+
+
+module.exports.home = async function (req, res) {
+    try {
+        let allProducts = await Product.find();
+
+        return res.render("home", {
+            title: "Home Page",
+            allProducts: allProducts
+        })
+
+    } catch (error) {
+        console.log("Inside catch of homecontroller and the error is :-", error);
+        return;
+    }
+
+}
+
+
+module.exports.filterProducts = async function (req, res) {
+    try {
+        const keyword = req.body.keyWord;
+        const regex = new RegExp(keyword, 'i');
+        let allProducts = await Product.find({ productName: regex });
+        // console.log("Inside filterProducts", req.body);
+        return res.render("home", {
+            title: "Home Page",
+            allProducts: allProducts
+        });
+    } catch (error) {
+        console.log("Inside catch of filterProducts and the error is :-", error);
+        return res.status(500).send("Internal Server Error");
+    }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express= require("express");
 const router=express.Router();
 const homeController=require("../controllers/home_controller");
+const { route } = require("./sellers");
 
 
 router.get('/',homeController.home);
 router.use('/sellers',require('./sellers'));
+router.post('/product/search',homeController.filterProducts);
 
 module.exports=router;
