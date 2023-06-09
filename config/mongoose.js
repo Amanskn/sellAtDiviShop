3e4ce5b6963ad79a77b2549c1b61f62e82deeefd
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-09 14:21:28 +0530

    removed process.env stuff

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_URL)
+mongoose.connect("mongodb+srv://29maddhesiaaman:McDhjreJknjbEPsy@cluster0.xfmjuxm.mongodb.net/")
   .then(() => console.log('Successfully!! connected to the database'))
   .catch((error) => {
     console.error('Error in connecting to the database and the error is :-', error);
