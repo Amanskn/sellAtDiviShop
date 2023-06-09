fd895efcce98127c0deb64895fd0375b92bcbc78
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-09 15:43:44 +0530

    update regarding mongoose version

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express=require("express");
 const cookieParser = require('cookie-parser')
 const app=express();
-const port=process.env.PORT||3000;
+const port=3000;
 const expressLayouts=require('express-ejs-layouts');
 const DB=require('./config/mongoose')
 app.use(cookieParser());
